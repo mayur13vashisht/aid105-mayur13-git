d20870f2ed4388e328e064691247232c58ae2bd9
mayur13vashisht <mayur13vashisht@gmail.com>
2026-01-12 18:21:47 +0530

    User Model Code in Python

---
+++ backend/models/user_model.py
@@ -0,0 +1,7 @@
+class User:
+    def __init__(self, name, age, income, state, category):
+        self.name = name
+        self.age = age
+        self.income = income
+        self.state = state
+        self.category = category
