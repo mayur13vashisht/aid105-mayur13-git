34fce34d5905a0bf6d66937e454c3259d0a875be
mayur13vashisht <mayur13vashisht@gmail.com>
2026-01-13 19:52:51 +0530

    Add Scheme class with initialization parameters

---
+++ backend/models/scheme_model.py
@@ -0,0 +1,9 @@
+class Scheme:
+    def __init__(self, name, min_income, max_income, min_age, max_age, state, category):
+        self.name = name
+        self.min_income = min_income
+        self.max_income = max_income
+        self.min_age = min_age
+        self.max_age = max_age
+        self.state = state
+        self.category = category
